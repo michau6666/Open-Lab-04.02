a1caff7e7c239fc1002cfec77b4a85b1dce934e6
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-10-13 08:40:14 +0200

    Update Doubler.cs

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string a = string.Empty;
+            for (int i = 0; i < original.Length; i++)
+            {
+                a += original[i];
+                a += original[i];
+            }
+
+            return a;
         }
     }
 }
